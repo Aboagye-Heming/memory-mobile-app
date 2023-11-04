09a5b0091478613ba7bb6cce1d22953424d02dbd
Aboagye=Heming <hemingkwabena@gmail.com>
2023-11-04 22:57:34 +0000

    added the memories page

---
+++ src/pages/MemoriesPage.vue
@@ -0,0 +1,40 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>All memories</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content>
+      <ion-list>
+        <ion-item>Kwabena </ion-item>
+        <ion-item>Aboagye </ion-item>
+        <ion-item>Heming </ion-item>
+      </ion-list>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script>
+import {
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonList,
+  IonItem,
+} from "@ionic/vue";
+
+export default {
+  components: {
+    IonPage,
+    IonHeader,
+    IonTitle,
+    IonContent,
+    IonToolbar,
+    IonList,
+    IonItem,
+  },
+};
+</script>
